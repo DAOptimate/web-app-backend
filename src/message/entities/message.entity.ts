dd0a520f23f3c373162328750df419955e294d27
subject026 <subject026@protonmail.com>
2022-08-29 23:33:06 +0100

    create message entity

--- src/message/entities/message.entity.ts
+++ src/message/entities/message.entity.ts
@@ -1,1 +1,25 @@
-export class Message {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'message' })
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  company: string;
+
+  @Column()
+  message: string;
+
+  @Column({ default: false })
+  isRead: boolean;
+
+  @Column({ default: new Date().getTime() / 1000 })
+  date: number;
+}
