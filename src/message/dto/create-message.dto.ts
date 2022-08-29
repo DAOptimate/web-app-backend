0cac5e69a7957e433657e86fad1558f64f994d04
subject026 <subject026@protonmail.com>
2022-08-29 23:34:24 +0100

    define create message dto

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,1 +1,18 @@
-export class CreateMessageDto {}
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateMessageDto {
+  @IsNotEmpty()
+  @Length(1, 100)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Length(1, 100)
+  company: string;
+
+  @IsNotEmpty()
+  @Length(1, 500)
+  message: string;
+}
