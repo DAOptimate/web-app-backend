7da35c4c30f89d055b825a5f2dc4b4edc0dbad72
subject026 <subject026@protonmail.com>
2022-08-30 10:02:54 +0100

    validation changes

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,18 +1,14 @@
 import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 
 export class CreateMessageDto {
-  @IsNotEmpty()
   @Length(1, 100)
   name: string;
 
-  @IsNotEmpty()
   @IsEmail()
   email: string;
 
-  @Length(1, 100)
   company: string;
 
-  @IsNotEmpty()
-  @Length(1, 500)
+  @Length(15, 500)
   message: string;
 }
