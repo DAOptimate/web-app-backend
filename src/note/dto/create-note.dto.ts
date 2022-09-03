4fcc3eb490a17bd583be6f441fb7659bce83692a
subject026 <subject026@protonmail.com>
2022-09-03 18:41:25 +0100

    setup entity & dto

--- src/note/dto/create-note.dto.ts
+++ src/note/dto/create-note.dto.ts
@@ -1,1 +1,9 @@
-export class CreateNoteDto {}
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class CreateNoteDto {
+  @IsNotEmpty()
+  messageId: string;
+
+  @IsNotEmpty()
+  content: string;
+}
--- src/note/entities/note.entity.ts
+++ src/note/entities/note.entity.ts
@@ -1,1 +1,28 @@
-export class Note {}
+import { IsNotEmpty } from 'class-validator';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'note' })
+export class Note {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsNotEmpty()
+  messageId: string;
+
+  //   @Column()
+  //   author: string;
+
+  @Column()
+  @IsNotEmpty()
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
