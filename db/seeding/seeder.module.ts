6653e0ae365c6d59d25dd78029d9b0344d090f47
subject026 <subject026@protonmail.com>
2022-09-04 20:35:25 +0100

    create seeder module

---
+++ db/seeding/seeder.module.ts
@@ -0,0 +1,44 @@
+import { faker } from '@faker-js/faker';
+import { Message } from '../../src/message/entities/message.entity';
+import { DataSource } from 'typeorm';
+
+export class SeederModule {
+  private static dataSource = new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3307,
+    username: 'root',
+    password: 'password',
+    database: 'daoptimate_dev_db',
+    entities: [Message],
+    synchronize: true,
+  });
+
+  public static async run() {
+    await this.dataSource.initialize();
+
+    const messageRepository = this.dataSource.getRepository(Message);
+
+    const messageData = [...Array(15)].map(() =>
+      messageRepository.create({
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        email: faker.internet.email(),
+        company: faker.company.name(),
+        message: faker.lorem.sentences(2),
+      }),
+    );
+
+    await Promise.all(
+      messageData.map((message) => {
+        return messageRepository.save(message);
+      }),
+    );
+  }
+
+  public static async clean() {
+    await this.dataSource.initialize();
+
+    // passing true here drops tables for registered entities before syncing
+    await this.dataSource.synchronize(true);
+  }
+}
