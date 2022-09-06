ed2067f974ce750343118483306f68f088ef1415
subject026 <subject026@protonmail.com>
2022-09-06 22:57:49 +0100

    remove unused code

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,17 +1,12 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  // Patch,
-  Param,
-  // Delete,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { CreateNoteDto } from './dto/create-note.dto';
-// import { UpdateNoteDto } from './dto/update-note.dto';
 
 @Controller('note')
 export class NoteController {
@@ -23,19 +18,4 @@ export class NoteController {
     const { messageId, ...createNoteParams } = createNoteDto;
     return this.noteService.create(+messageId, createNoteParams);
   }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.noteService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-  //   return this.noteService.update(+id, updateNoteDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.noteService.remove(+id);
-  // }
 }
