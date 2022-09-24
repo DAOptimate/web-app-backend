16ee7d1fb842517fd4070fed3df99d017981c766
subject026 <subject026@protonmail.com>
2022-09-24 12:39:57 +0100

    switch back to seperate env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,30 @@ import dbConfig from './config/db.config';
 import { MessageModule } from './message/message.module';
 import { NoteModule } from './note/note.module';
 
+const getEnvFilePath = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return '.dev.env';
+    case 'testing':
+      return '.test.env';
+    case 'production':
+      return '.prod.env';
+    default:
+      console.log('no NODE_ENV set.... exiting!');
+      process.exit();
+  }
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: getEnvFilePath(),
       load: [dbConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
         const databaseConfig = await configService.get('database');
-        console.log({ databaseConfig });
         return {
           ...databaseConfig,
         };
